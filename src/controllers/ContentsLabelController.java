f9093197d4744fe70d37456ed5848b28467cd1ec
Yahel Nachum <ynachum@wpi.edu>
2015-04-23 18:44:51 -0400

    Update

--- src/controllers/ContentsLabelController.java
+++ src/controllers/ContentsLabelController.java
@@ -1,14 +1,12 @@
 package controllers;
 
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.JFrame;
 
 import boundries.Application;
 import boundries.ContentsLabel;
 
-public class ContentsLabelController implements MouseListener{
+public class ContentsLabelController extends MouseAdapter{
 
 	Application app;
 	ContentsLabel cl;
@@ -22,25 +20,4 @@ public class ContentsLabelController implements MouseListener{
 		app.getScrollBarPanel().changeScrollBarContentsPanel(cl.getNode().getChildren(), cl.getNode().getChildrensLevel());
 		app.updateControllers();
 	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
